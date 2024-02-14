c9b07535175623e6027bb00b0277e8305ed31363
yusufiii000 <158528545+yusufiii000@users.noreply.github.com>
2024-02-14 15:25:52 +0300

    gir.py

---
+++ gir.py
@@ -0,0 +1,80 @@
+#Script By Yusufi Hack Turkish
+
+print('Tool Başlıyor')
+print('')
+
+import sys
+import time
+
+def loading_animation():
+    chars = "YÜKLENİYOR"
+    for i in range(20):
+        sys.stdout.write("\r" + chars[i % len(chars)] + " Bekleyin...")
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+loading_animation()
+
+import os 
+os.system('clear')
+
+
+print('Turkish-Kendi Host Adresimiz')
+print('')
+a=int(input('Bulmak İçin 1 e Tıklayınız == '))
+print('')
+print('Bulunuyor...')
+print('')
+
+import sys
+import time
+
+def loading_animation():
+    chars = "1 Tane"
+    for i in range(20):
+        sys.stdout.write("\r" + chars[i % len(chars)] + " Bulunuyor...")
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+loading_animation()
+print('')
+print('')
+
+print("Bulunmuştur...")
+
+
+a==1 
+
+
+import socket
+
+
+hostnm = socket.gethostname()
+ipaddr = socket.gethostbyname(hostnm)
+
+print("\nKendi LocalHost Adresim:", ipaddr)
+
+
+print('Cıkış Yapılıyor')
+print('')
+
+import sys
+import time
+
+def loading_animation():
+    chars = "@@-/\|<>"
+    for i in range(20):
+        sys.stdout.write("\r" + chars[i % len(chars)] + " Çıkış Yapılıyor...")
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+loading_animation()
+print('')
+print('')
+print('Çıkıldı')
+
+import os 
+os.system('clear')
+
+
+
